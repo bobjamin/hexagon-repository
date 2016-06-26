be3dda592f43ead9942162e9effb2a50747b11ba
Chris Barbour <xbobjamin@gmail.com>
2016-06-26 15:46:03 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,21 @@
+CMD="$1"
+LOOKUP="$2"
+
+isInFile ()
+{
+  FILE="$1"
+  LOOKUP="$2"
+  while read line; do
+    set -- junk $line
+    shift
+    left=$1
+    right=$2
+    if [ "$LOOKUP" == "$1" ]; then
+      echo $right;
+      return 0;
+    fi
+  done <"$FILE"
+}
+if [ "$CMD" == "lookup" ]; then
+  isInFile /repo.txt "$LOOKUP" 
+fi
